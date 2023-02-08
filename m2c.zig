610a180092787c3c431ad36dc24a8a79e92ef1b7
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-02-08 09:01:18 +0100

    include 'envs_dirs' and 'conda_prefix' into json

--- m2c.zig
+++ m2c.zig
@@ -10,10 +10,6 @@ const File = std.fs.File;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const alct = gpa.allocator();
 
-const CondaInfo = struct {
-    envs: [][]const u8,
-};
-
 const logFilePath = "/tmp/m2c";
 
 fn fileExists(path: [*:0]const u8) bool {
@@ -44,6 +40,15 @@ fn logCliArguments() !void {
     try file.writeAll("\n");
 }
 
+fn runCommand(args: []const []const u8) ![]u8 {
+    const res = try std.ChildProcess.exec(.{
+        .allocator = alct,
+        .argv = args,
+    });
+
+    return res.stdout;
+}
+
 fn runMicromambaEnvs() ![]u8 {
     const args = [_][]const u8{ "micromamba", "--json", "env", "list" };
 
@@ -56,14 +61,17 @@ fn runMicromambaEnvs() ![]u8 {
 }
 
 fn getEnvironments() ![][]const u8 {
-    var jsonStr = try runMicromambaEnvs();
+    // run micromamba command
+    const args = [_][]const u8{ "micromamba", "--json", "env", "list" };
+    var jsonStr = try runCommand(&args);
 
+    // parse micromamba json output
     var parser = std.json.Parser.init(alct, false);
     var tree = try parser.parse(jsonStr);
     var envsDict = tree.root.Object.get("envs").?;
 
+    // convert envs json dict to list of strings
     var envs = ArrayList([]const u8).init(alct);
-
     for (envsDict.Array.items) |item| {
         try envs.append(item.String);
     }
@@ -71,9 +79,49 @@ fn getEnvironments() ![][]const u8 {
     return envs.items;
 }
 
-fn writeCondaInfoJson(envs: [][]const u8) !void {
+fn getEnvironmentsDirs() ![][]const u8 {
+    // run micromamba command
+    const args = [_][]const u8{ "micromamba", "--json", "config", "list" };
+    var jsonStr = try runCommand(&args);
+
+    // parse micromamba json output
+    var parser = std.json.Parser.init(alct, false);
+    var tree = try parser.parse(jsonStr);
+    var envsDirsDict = tree.root.Object.get("envs_dirs").?;
+
+    // convert envs json dict to list of strings
+    var envs = ArrayList([]const u8).init(alct);
+    for (envsDirsDict.Array.items) |item| {
+        try envs.append(item.String);
+    }
+
+    return envs.items;
+}
+
+fn getBaseEnvPath() ![]const u8 {
+    // run micromamba command
+    const args = [_][]const u8{ "micromamba", "--json", "info" };
+    var jsonStr = try runCommand(&args);
+
+    // parse micromamba json output
+    var parser = std.json.Parser.init(alct, false);
+    var tree = try parser.parse(jsonStr);
+    var baseEnvPath = tree.root.Object.get("base environment").?;
+
+    return baseEnvPath.String;
+}
+
+fn writeCondaInfoJson(envs: [][]const u8, envsDirs: [][]const u8, baseEnvPath: []const u8) !void {
+    const CondaInfo = struct {
+        conda_prefix: []const u8,
+        envs: [][]const u8,
+        envs_dirs: [][]const u8,
+    };
+
     const condaInfo = CondaInfo{
+        .conda_prefix = baseEnvPath,
         .envs = envs,
+        .envs_dirs = envsDirs,
     };
 
     const stdout = std.io.getStdOut().writer();
@@ -87,6 +135,8 @@ fn writeCondaInfoJson(envs: [][]const u8) !void {
 pub fn main() !void {
     try logCliArguments();
     var envs = try getEnvironments();
+    var envsDir = try getEnvironmentsDirs();
+    var baseEnvPath = try getBaseEnvPath();
 
-    try writeCondaInfoJson(envs);
+    try writeCondaInfoJson(envs, envsDir, baseEnvPath);
 }
